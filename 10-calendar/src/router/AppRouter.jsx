07903e63c5ac397dddd249f8aa9d59af72f93461
yinethriv <karinariv0311@gmail.com>
2025-02-20 11:28:13 -0500

    ultimos cambios

--- 10-calendar/src/router/AppRouter.jsx
+++ 10-calendar/src/router/AppRouter.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router";
 import { LoginPage } from "../auth/pages/LoginPage";
 import { CalendarPage } from "../calendar/pages/CalendarPage";
+import { getEnvVariables } from "../helpers/getEnvVariables";
 
 export const AppRouter = () => {
   const authStatus = "authenticated";
+
 
   return (
     <Routes>
